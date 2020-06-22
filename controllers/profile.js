258549383dd10d894e7e69ac44c99732398c21ed
Kegbi <kegbi.networking@gmail.com>
2020-06-22 06:22:51 +0300

    Implemented basic profile functions with changing DB data

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,16 +1,34 @@
 const handleProfileGet = (req, res, db) => {
-    const {id} = req.params;
-    db.select('*').from('users').where({id})
-        .then(user => {
-            if (user.length) {
-                res.json(user[0])
-            } else {
-                res.status(400).json('Not found')
-            }
-    })
-    .catch(err => res.status(400).json('Error getting user'))
+	const {id} = req.params;
+	db.select('*').from('users').where({id})
+	.then(user => {
+		if (user.length) {
+			res.json(user[0]);
+		} else {
+			res.status(400).json('Not found');
+		}
+	})
+	.catch(err => res.status(400).json('Error getting user'));
 }
 
+const handleProfileUpdate = (req, res, db) => {
+	console.log(req.params.id)
+	const { id } = req.params;
+	const { name, age, pet } = req.body.formInput;
+	db('users')
+		.where({ id: id})
+		.update({name: name, age: age, pet: pet})
+		.then(resp => {
+			if (resp) {
+				res.json("Success")
+			} else {
+				res.status(400).json('Unable to update')
+			}
+		})
+		.catch(err => res.status(400).json('Error updating profile'))
+};
+
 module.exports = {
-    handleProfileGet
+	handleProfileGet,
+	handleProfileUpdate
 };
--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,7 +1,8 @@
 BEGIN TRANSACTION;
 
 -- User 'wes' with password 'wes'
-INSERT into users (name, email, entries, joined) values ('wes', 'wes@gmail.com', '15', '2020-02-02');
+INSERT into users (name, email, pet, age, entries, joined) values ('wes', 'wes@gmail.com', 'Drago', '23', '15',
+'2020-02-02');
 INSERT into login (hash, email) values ('$2a$10$d649oACgoHFUDg2fnGqBzeiwnm6xfwfnembKEQlTydbK2ZlZUgWfm', 'wes@gmail.com')
 ;
 
